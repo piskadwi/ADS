860947d5b64d46548de90cb7888cf1b1cafe2a88
piskadwi <36060062+piskadwi@users.noreply.github.com>
2018-05-06 11:45:18 +0700

    Update konekdb.php

--- ADS/konekdb.php
+++ ADS/konekdb.php
@@ -3,10 +3,14 @@ $host ="localhost";
 $user ="root";
 $password="";
 $db="endorse";
-$konek=mysqli_connect($host, $user, $password,$db);
-if ($konek){
-	//echo "BERHASIL!";
+$konek=mysqli_connect($host,$user,$password);
+if (!$konek){
+	echo "GAGAL TERHUBUNG!".mysqli_connect_error();
+	exit();
 	}else{
-		echo "GAGAL!";
+		$pilihdb=mysqli_select_db($konek,$db);
+		if(!$pilihdb){
+			echo "GAGAL TERHUBUNG KE DB".mysqli_connect_error();
+			}
 		}
 ?>
