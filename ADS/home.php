fab350c7a4591ddcf8cc61dce5eb0fc5a5a57565
piskadwi <36060062+piskadwi@users.noreply.github.com>
2018-05-07 00:04:49 +0700

    Add files via upload

---
+++ ADS/home.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+    <header></header>
+    <div class="container"><img src="assets/img/kategori.jpg"></div>
+    <div class="container">
+        <div class="col-sm-2">
+            <p class="text-center">Fashion<img class="img-circle" src="assets/img/81vL0zMgUzL._UL1500_.jpg" width="100"></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="text-center">Momen Acara<img class="img-circle" src="assets/img/event.png" width="100"></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="text-center">Elektronik <img class="img-circle" src="assets/img/elektronik.png" width="100"></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="text-center">Kuliner <img class="img-circle" src="assets/img/kuliner.png" width="100"></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="text-center">Wisata <img class="img-circle" src="assets/img/travel.png" width="100"></p>
+        </div>
+        <div class="col-sm-2">
+            <p class="text-center">Pakaian <img class="img-circle" src="assets/img/otomotif.png" width="100"></p>
+        </div>
+    </div>
+    <div class="container"><img src="assets/img/profile.jpg"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/download (2).jpg">
+                    <div class="caption">
+                        <h4 class="text-uppercase">bayu skak</h4>
+                        <p class="text-lowercase text-left">Bayu Eko Moektito adalah seorang aktor, komedian dan personalia YouTube berkebangsaan Indonesia </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/images.jpg">
+                    <div class="caption">
+                        <h4 class="text-uppercase">raditya d</h4>
+                        <p class="text-lowercase text-left">Raditya Dika adalah seorang penulis, komedian, sutradara &amp; aktor. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/images_thumb.jpg">
+                    <div class="caption">
+                        <h4 class="text-uppercase">Raisa </h4>
+                        <p class="text-lowercase text-left">Raisa adalah seorang penyanyi berkebangsaan Indonesia </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/download (2).png">
+                    <div class="caption">
+                        <h4 class="text-uppercase">inspired27 </h4>
+                        <p class="text-lowercase text-left">Keep The Faith On Process - Premium Clothing Indonesia </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/download (3).jpg">
+                    <div class="caption">
+                        <h4 class="text-uppercase">infia_fact </h4>
+                        <p class="text-lowercase text-left">nikmati pengalaman baru browsing di instagram</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-2">
+                <div class="thumbnail"><img src="assets/img/com.myvideo.calonsarjana.png">
+                    <div class="caption">
+                        <h4 class="text-uppercase">calon sarjana</h4>
+                        <p class="text-lowercase text-left">channel youtube yang membahas fakta kontroversial, aneh, &amp; keren</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
